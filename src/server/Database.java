7f99d7b62e481fa60fe09035772596ae0e9283b6
Marvin Talahaturusun <marvin_b_l@hotmail.com>
2014-10-10 23:58:14 +0200

    Added instructions in database.java class

--- src/server/Database.java
+++ src/server/Database.java
@@ -6,13 +6,25 @@ import com.mysql.jdbc.Connection;
 
 public class Database {
 	
+	/*
+	 * TODO before running app
+	 * 1. import library mysql-connector/j (jar file found on mysql.org)
+	 * 2. import unwdmi.sql to database
+	 * 3. create user using the following statements in mysql
+	 * 		CREATE USER 'unwdmi'@'localhost' IDENTIFIED BY 'unwdmi';
+	 * 		GRANT ALL PRIVILEGES ON unwdmi.* TO 'unwdmi'@'localhost';
+	 * 		FLUSH PRIVILEGES;
+	 * 4. run app! :)
+	 */
+	
 	private final String DATABASE = "unwdmi";
-	private final String USERNAME = "root";
-	private final String PASSWORD = "";
+	private final String USERNAME = "unwdmi";
+	private final String PASSWORD = "unwdmi";
 	
 	private Connection conn;
 	
 	public Database() {
+		//when instantiating this class, immediately create a connection with the database.
 		this.conn = connect();
 	}
 	
@@ -24,10 +36,10 @@ public class Database {
 		}
 		
 		try {
+			//try to make a connoction with the database using config data on top of this page
 			Connection conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/" + DATABASE + "?user=" + USERNAME + "&password=" + PASSWORD);
 			return conn;
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
@@ -35,11 +47,11 @@ public class Database {
 	
 	public void insertMeasurement(int stn, String date, String time, double temp, double dewp, double stp, double slp, double visib, double wdsp, double prcp, double sndp, String frshtt, double cldc, int wnddir) {
 		try {
+			//insert formatted data into the database
 			Statement s = conn.createStatement();
 			s.executeUpdate("INSERT INTO measurements (stn, date, time, temp, dewp, stp, slp, visib, wdsp, prcp, sndp, frshtt, cldc, wnddir) "
 					+ "VALUES (" + stn + ", '" + date + "', '" + time + "', " + temp + ", " + dewp + ", " + stp + ", " + slp + ", " + visib + ", " + wdsp + ", " + prcp + ", " + sndp + ", '" + frshtt + "', " + cldc + ", " + wnddir + ")");
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -47,9 +59,9 @@ public class Database {
 	
 	public void disconnect() {
 		try {
+			//make sure the connection can be closed again
 			conn.close();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
